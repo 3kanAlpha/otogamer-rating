62b1728dd3b16d46af96cd9805f18ef8c028f6fa
3kanAlpha <39447635+3kanAlpha@users.noreply.github.com>
2024-03-05 22:34:33 +0900

    add vuetify, mathjax

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # 音ゲーマーレーティング
 いろんな音ゲーを横断してレーティングを計算したい
 
+## レーティング計算式
+工事中
+
 # Nuxt 3 Minimal Starter
 
 Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,50 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    //...
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  app: {
+    head: {
+      "meta": [
+        {
+          "name": "viewport",
+          "content": "width=device-width, initial-scale=1"
+        },
+        {
+          "charset": "utf-8"
+        }
+      ],
+      "link": [],
+      "style": [],
+      "script": [
+        {
+          src: "https://polyfill.io/v3/polyfill.min.js?features=es6"
+        },
+        {
+          src: "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js",
+          id: "MathJax-script",
+          async: true
+        }
+      ],
+      "noscript": []
+    }
+  }
 })
